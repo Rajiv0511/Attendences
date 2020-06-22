7a586493b443ae477bad7a4cc159bb15bf1524c6
Rajiv.M <yuv.rajiv93@gmail.com>
2020-06-22 13:05:31 +0530

    third commit

--- app/src/main/java/com/eleven/mr/attendence/Dashboard.java
+++ app/src/main/java/com/eleven/mr/attendence/Dashboard.java
@@ -1,16 +1,20 @@
 package com.eleven.mr.attendence;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Dashboard extends AppCompatActivity {
+
+    private TextView textGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        textGrid = findViewById(R.id.textGrid);
 
     }
 }
